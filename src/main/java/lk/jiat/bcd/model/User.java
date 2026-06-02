4b3ac71c7fc298793ef920387ed86e378bf30a73
chanuka-rukshan <chanukarukshan81@gmail.com>
2026-06-02 21:19:41 +0530

    Done

--- src/main/java/lk/jiat/bcd/model/User.java
+++ src/main/java/lk/jiat/bcd/model/User.java
@@ -5,4 +5,22 @@ public class User {
     private String name;
     private String mobile;
 
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
